fe24f470786c06b93e0f55fdd2806c53992f7e63
Mazlum Emre Girgin <mazlumemregirgin@msn.com>
2023-08-27 11:54:55 +0300

    inputs chose

---
+++ main.js
@@ -0,0 +1,11 @@
+const boxWidth = document.querySelector("#width")
+const boxHeight = document.querySelector("#height")
+const boxColour = document.querySelector("#box-colour-selector")
+const boxBorderType = document.querySelector("#box-border-type-selector")
+const boxBorderWidth = document.querySelector("#border-width")
+const boxBorderColour = document.querySelector("#box-border-color-selector")
+const boxRotate = document.querySelector("#box-rotate")
+const applyButton = document.querySelector("#apply-button")
+
+
+
